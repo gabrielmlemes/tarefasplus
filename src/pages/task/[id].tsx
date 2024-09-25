cb15d82bb11ae040172886c1b5b1f53a5e1fe739
Gabriel Lemes <g.moreiralemess@gmail.com>
2024-09-25 20:06:07 -0300

    Adicionando comentários no banco

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -2,10 +2,56 @@ import Head from "next/head";
 import styles from "./styles.module.css";
 import { GetServerSideProps } from "next";
 
-import { db } from "@/src/services/firebaseConnection";
-import { collection, doc, where, query, getDoc } from "firebase/firestore";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useSession } from "next-auth/react";
 
-const Task = () => {
+import { db } from "@/src/services/firebaseConnection";
+import {
+  collection,
+  doc,
+  where,
+  query,
+  getDoc,
+  addDoc,
+} from "firebase/firestore";
+import Textarea from "@/src/components/textarea";
+
+interface TaskProps {
+  item: {
+    tarefa: string;
+    created: string;
+    public: boolean;
+    user: string;
+    taskId: string;
+  };
+}
+
+const Task = ({ item }: TaskProps) => {
+  const [input, setInput] = useState("");
+  const { data: session } = useSession();
+
+  async function handleComment(e: FormEvent) {
+    e.preventDefault();
+
+    if (input === "") return;
+
+    if (!session?.user?.email || !session?.user?.name) return;
+
+    try {
+      const docRef = await addDoc(collection(db, "comments"), {
+        comentario: input,
+        created: new Date(),
+        user: session?.user?.email,
+        name: session?.user?.name,
+        taskId: item?.taskId,
+      });
+
+      setInput("");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,13 +60,38 @@ const Task = () => {
 
       <main className={styles.main}>
         <h1>Tarefa</h1>
+        <article className={styles.task}>
+          <p>{item?.tarefa}</p>
+        </article>
+
+        <section className={styles.commentsContainer}>
+          <h2>Fazer comentário</h2>
+
+          <form onSubmit={handleComment}>
+            <Textarea
+              placeholder="Digite seu comentário..."
+              value={input}
+              onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+                setInput(e.target.value)
+              }
+            />
+
+            <button
+              className={styles.button}
+              disabled={!session?.user}
+              type="submit"
+            >
+              Enviar comentário
+            </button>
+          </form>
+        </section>
       </main>
     </div>
   );
 };
 
 // No serverSide, busca os dados e trata (no objeto 'task') para devolver como prop pro componente
-const getserverSideProps: GetServerSideProps = async ({ params }) => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const id = params?.id as string;
 
   const docRef = doc(db, "tarefas", id);
@@ -36,6 +107,7 @@ const getserverSideProps: GetServerSideProps = async ({ params }) => {
     };
   }
 
+  //    Redireciona o usuário para a home caso o id pesquisado não seja público
   if (!snapshot.data()?.public) {
     return {
       redirect: {
@@ -56,7 +128,9 @@ const getserverSideProps: GetServerSideProps = async ({ params }) => {
   };
 
   return {
-    props: {},
+    props: {
+      item: task,
+    },
   };
 };
 
